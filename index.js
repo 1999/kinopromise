dd247ed0fb6e1ae6f7008de0ae84e7a828655beb
Dmitry Sorin <dsorin@yandex-team.ru>
2016-03-05 02:24:31 +0300

    Port code from master branch

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 'use strict';
 
-module.exports = (process.release && process.release.name === 'node')
-    ? require('./lib/es2015')
-    : require('./lib/es5');
+class KinoPromise extends Promise {
+    spread(onFulfilled, onRejected) {
+        function onFulfilledInternal(res) {
+            if (Array.isArray(res)) {
+                return onFulfilled(...res);
+            }
+        };
+
+        return this.then(onFulfilledInternal, onRejected);
+    }
+}
+
+KinoPromise.all = function KinoPromise_static_all(promises) {
+    if (arguments.length > 1 || typeof promises !== 'object') {
+        return Promise.all.apply(Promise, arguments);
+    }
+
+    return new KinoPromise((resolve, reject) => {
+        const isPromisesList = Array.isArray(promises);
+        let promisesArray;
+        let promisesKeys;
+
+        if (isPromisesList) {
+            promisesArray = promises;
+        } else {
+            promisesKeys = Object.keys(promises);
+            promisesArray = promisesKeys.map(key => promises[key]);
+        }
+
+        Promise.all(promisesArray).then(res => {
+            // transform output into an object
+            let output;
+
+            if (isPromisesList) {
+                output = res;
+            } else {
+                output = res.reduce((output, chunk, index) => {
+                    output[promisesKeys[index]] = chunk;
+                    return output;
+                }, {});
+            }
+
+            resolve(output);
+        }).catch(reject);
+    });
+};
+
+export default KinoPromise;
