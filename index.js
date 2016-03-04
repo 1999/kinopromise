54e1519785be3abdb4282c342969f6dfda259d68
Dmitry Sorin <dsorin@yandex-team.ru>
2016-03-05 02:24:31 +0300

    Use ES2015 code everywhere

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
 'use strict';
 
-module.exports = (process.release && process.release.name === 'node')
-    ? require('./lib/es2015-build')
-    : require('./lib/es5');
+function *objectIterator() {
+    for (let key in this) {
+        if (typeof this[key] === 'object' && this[key].then) {
+            yield this[key].then(res => [key, res]);
+        } else {
+            yield [key, res];
+        }
+    }
+}
+
+function convertToObject(data) {
+    let output = {};
+
+    for (let [key, value] of data) {
+        output[key] = value;
+    }
+
+    return output;
+}
+
+class KinoPromise extends Promise {
+    spread(onFulfilled, onRejected) {
+        let onFulfilledInternal = function (res) {
+            if (Array.isArray(res)) {
+                return onFulfilled(...res);
+            }
+        };
+
+        return this.then(onFulfilledInternal, onRejected);
+    }
+}
+
+KinoPromise.all = function KinoPromise_static_all(promises) {
+    if (arguments.length > 1 || typeof promises !== 'object') {
+        return Promise.all.apply(Promise, arguments);
+    }
+
+    return new KinoPromise((resolve, reject) => {
+        const isIterable = (promises[Symbol.iterator] !== undefined);
+
+        if (!isIterable) {
+            promises[Symbol.iterator] = objectIterator;
+        }
+
+        Promise.all(promises).then(output => {
+            if (!isIterable) {
+                output = convertToObject(output);
+                delete promises[Symbol.iterator];
+            }
+
+            resolve(output);
+        }).catch(reject);
+    });
+};
+
+export default KinoPromise;
--- test/api.js
+++ test/api.js
@@ -1,37 +1,33 @@
 'use strict';
 
-var chai = require('chai');
+const chai = require('chai');
+const expect = chai.expect;
+const KinoPromise = require('../');
 
-var expect = chai.expect;
-var KinoPromise = require('../');
-var Promise = (process.release && process.release.name === 'node')
-    ? global.Promise
-    : require('pinkie');
-
-describe('API interface tests', function () {
-    it('global variables should exist', function () {
+describe('API interface tests', () => {
+    it('global variables should exist', () => {
         expect(KinoPromise).to.exist;
         expect(KinoPromise).to.be.a('function');
     });
 
-    it('should inherit from Promise', function () {
-        var obj = new KinoPromise(function () {});
+    it('should inherit from Promise', () => {
+        const obj = new KinoPromise(() => undefined);
 
         expect(obj).to.be.instanceof(KinoPromise);
         expect(obj).to.be.instanceof(Promise);
     });
 
-    it('should take strict number of arguments', function () {
-        var obj = new KinoPromise(function () {});
+    it('should take strict number of arguments', () => {
+        const obj = new KinoPromise(() => undefined);
 
         expect(obj.spread).to.have.length.within(1, 2);
         expect(KinoPromise.all).to.have.length(1);
     });
 
-    it('KinoPromise.prototype.spread should invoke callback with resolved array like Function.prototype.apply does', function () {
-        var promise = KinoPromise.all([
-            new KinoPromise(function (resolve) { resolve(1) }),
-            new KinoPromise(function (resolve) { resolve(2) })
+    it('KinoPromise.prototype.spread should invoke callback with resolved array like Function.prototype.apply does', () => {
+        const promise = KinoPromise.all([
+            new KinoPromise(resolve => resolve(1)),
+            new KinoPromise(resolve => resolve(2))
         ]);
 
         expect(promise).to.be.instanceof(KinoPromise);
@@ -43,10 +39,10 @@ describe('API interface tests', function () {
         });
     });
 
-    it('should allow object passing inside KinoPromise.all', function () {
-        var promise = KinoPromise.all({
-            foo: new KinoPromise(function (resolve) { resolve(1) }),
-            bar: new KinoPromise(function (resolve) { resolve(2) })
+    it('should allow object passing inside KinoPromise.all', () => {
+        const promise = KinoPromise.all({
+            foo: new KinoPromise(resolve => resolve(1)),
+            bar: new KinoPromise(resolve => resolve(2))
         });
 
         expect(promise).to.be.instanceof(KinoPromise);
@@ -58,33 +54,30 @@ describe('API interface tests', function () {
         });
     });
 
-    it('KinoPromise.prototype.spread should invoke onRejected callback if smth goes wrong', function () {
-        var promise = KinoPromise.all([
-            new KinoPromise(function (resolve, reject) { reject(new Error('error')) }),
-            new KinoPromise(function (resolve) { resolve(2) })
+    it('KinoPromise.prototype.spread should invoke onRejected callback if smth goes wrong', () => {
+        const promise = KinoPromise.all([
+            new KinoPromise((resolve, reject) => reject(new Error('error'))),
+            new KinoPromise(resolve => resolve(2))
         ]);
 
-        return promise.spread(function () {}, function () {
+        return promise.spread(() => undefined, function () {
             expect(arguments).to.have.length(1);
             expect(arguments[0]).to.be.instanceof(Error);
         });
     });
 
-    it('KinoPromise.prototype.spread should provide chainable data', function () {
-        var promise = KinoPromise.all([
-            new KinoPromise(function (resolve, reject) { resolve(1) }),
-            new KinoPromise(function (resolve) { resolve(2) })
+    it('KinoPromise.prototype.spread should provide chainable data', () => {
+        const promise = KinoPromise.all([
+            new KinoPromise(resolve => resolve(1)),
+            new KinoPromise(resolve => resolve(2))
         ]);
 
-        return promise.spread(function (one, two) {
-            return {
-                foo: one,
-                bar: two
-            };
+        return promise.spread((one, two) => {
+            return {one, two};
         }).then(function () {
             expect(arguments).to.have.length(1);
-            expect(arguments[0].foo).to.equal(1);
-            expect(arguments[0].bar).to.equal(2);
+            expect(arguments[0].one).to.equal(1);
+            expect(arguments[0].two).to.equal(2);
         });
     });
 });
